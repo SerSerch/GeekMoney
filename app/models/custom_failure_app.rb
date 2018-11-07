a1bf323760bb229e5d57d758d24465ffac982307
andech <7578191@gmail.com>
2018-11-08 00:44:43 +0300

    Comment failureapp

--- app/models/custom_failure_app.rb
+++ app/models/custom_failure_app.rb
@@ -1,24 +1,24 @@
 class CustomFailureApp < Devise::FailureApp
 
-  def redirect_url
-    byebug
-    json_error_response
-  end
+  # def redirect_url
+  #   byebug
+  #   json_error_response
+  # end
 
-  def respond
-    byebug
-    if attempted_path == "/api/v1/signup"
+  # def respond
+  #   byebug
+  #   if attempted_path == "/api/v1/signup"
       
-      json_error_response
-    else
-      super
-    end
-  end
+  #     json_error_response
+  #   else
+  #     super
+  #   end
+  # end
 
-  def json_error_response
-    self.status = 404
-    self.content_type = "application/json"
-    self.response_body = [ { message: i18n_message } ].to_json
-  end
+  # def json_error_response
+  #   self.status = 404
+  #   self.content_type = "application/json"
+  #   self.response_body = [ { message: i18n_message } ].to_json
+  # end
 
 end
