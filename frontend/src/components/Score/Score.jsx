17fdc06a44cca1782828b772efa6fd5a57b24831
serserch <serserch@yandex.ru>
2018-11-17 23:31:49 +0200

    Add score

--- frontend/src/components/Score/Score.jsx
+++ frontend/src/components/Score/Score.jsx
@@ -1,16 +1,68 @@
 import './Score.scss';
 
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import Radio from '@material-ui/core/Radio';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+
+import MonetizationOn from '@material-ui/icons/MonetizationOn';
+import AddIcon from '@material-ui/icons/Add';
+import Remove from '@material-ui/icons/Remove';
+import CompareArrows from '@material-ui/icons/CompareArrows';
+
+import { handleCheckboxChange } from 'containers/handleChange';
 
 class Score extends PureComponent {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            value: 'score2',
+        };
     }
-  
+
+    onHandleCheckChange = (event) => {
+        handleCheckboxChange(event, this);
+        console.log(event.target.name);
+    };
+
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+    };
+
     render() {
         return (
-            <div className="score">Score</div>
+            <Fragment>
+                <List className="trans-list _panel" component="div">
+                    {[1, 2].map(value => (
+                        <ListItem key={value} button component="label" htmlFor={`score${value}`}>
+                            <MonetizationOn fontSize="large" />
+                            <ListItemText primary={`Score ${value}`} />
+                            <Radio
+                                checked={this.state.value.indexOf(value) !== -1}
+                                onChange={this.handleChange}
+                                value={`score${value}`}
+                                id={`score${value}`}
+                                name="score"
+                                aria-label={`score${value}`}
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+                <div className="transactions">
+                    <Button variant="fab" color="primary" aria-label="Add">
+                        <AddIcon />
+                    </Button>
+                    <Button variant="fab" aria-label="Edit" >
+                        <CompareArrows />
+                    </Button>
+                    <Button variant="fab" color="secondary" aria-label="Edit">
+                        <Remove />
+                    </Button>
+                </div>
+            </Fragment>
         );
     }
 }
