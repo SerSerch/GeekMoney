e3f1226ad0361355e5a731366529195761a09a9a
serserch <serserch@yandex.ru>
2018-11-17 23:30:53 +0200

    Add category

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 import userReducer from './users';
 import scoreReducer from './scores';
+import categoryReducer from './categories';
 
 export default combineReducers({
     //за управление этим хранилищем отвечает этот редюсер
     user: userReducer,
     score: scoreReducer,
+    category: categoryReducer,
 });
